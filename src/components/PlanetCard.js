2ddaa2a455108853bd2b9902df1deabde791d15c
Victor Mendes <victorutdsim7@gmail.com>
2022-08-18 15:16:49 -0300

    requisito 5 [completo]: cria componente PlanetCard

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    const planetDescription = `Planeta ${planetName}`;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">
+          {planetName}
+        </p>
+        <img src={ planetImage } alt={ planetDescription } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
+
+export default PlanetCard;
