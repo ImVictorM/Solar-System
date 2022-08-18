28e647534a1b8d2085bcff0069788a9ee5bb3bcf
Victor Mendes <victorutdsim7@gmail.com>
2022-08-18 15:53:38 -0300

    requisito 10 [completo]: renderiza missões no component Missions usando o component MissionCard

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends Component {
   render() {
+    const missionList = missions.map((mission) => {
+      const { name, year, country, destination } = mission;
+      return (<MissionCard
+        name={ name }
+        year={ year }
+        country={ country }
+        destination={ destination }
+        key={ name }
+      />);
+    });
     return (
       <div>
         <Title headline="Missões" />
-        <div data-testid="missions" />
+        <div data-testid="missions">{missionList}</div>
       </div>
     );
   }
