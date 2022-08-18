127cdbb8f3cd7fa26e352f9825b0e35619160d2d
Victor Mendes <victorutdsim7@gmail.com>
2022-08-18 15:28:25 -0300

    requisito 6 [completo]: renderiza planetas no componente SolarSystem

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
+    const planetList = planets.map((planet) => {
+      const { name, image } = planet;
+      return <PlanetCard key={ name } planetName={ name } planetImage={ image } />;
+    });
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planetList}
       </div>
     );
   }
